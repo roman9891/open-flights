8b6a8814077fdb4b63b2d37acd1f36c8c6f5f98a
Roman Opalacz <roman9891@gmail.com>
2022-08-05 09:38:16 -0400

    fixed inheritance

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -1,6 +1,6 @@
 module Api
     module V1 
-        class AirlinesController < ApplicationRecord
+        class AirlinesController < ApplicationController
             def index
                 airlines = Airline.all
 
